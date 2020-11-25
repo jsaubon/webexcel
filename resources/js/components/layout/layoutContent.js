926b3e4318f1eee00487158ad3238e81a0a1107e
Joshua Saubon <joshuasaubon@gmail.com>
2020-11-26 06:11:06 +0800

    fix

--- resources/js/components/layout/layoutContent.js
+++ resources/js/components/layout/layoutContent.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useContext } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Layout, Breadcrumb } from "antd";
-import LayoutHeader from "./LayoutHeader";
-import LayoutFooter from "./LayoutFooter";
+import LayoutHeader from "./layoutHeader";
+import LayoutFooter from "./layoutFooter";
 import PageUsers from "../pages/private/pageUsers/pageUsers";
 import PageDashboard from "../pages/private/pageDashboard/pageDashboard";
 
@@ -21,7 +21,11 @@ const LayoutContent = () => {
                     style={{ padding: 24, minHeight: 380 }}
                 >
                     <Switch>
-                        <Route exact path="/dashboard" component={PageDashboard} />
+                        <Route
+                            exact
+                            path="/dashboard"
+                            component={PageDashboard}
+                        />
                         <Route exact path="/users" component={PageUsers} />
                         <Route path="/" exact>
                             <Redirect to="/dashboard" />
